4384d1a919be90ac65aa261d8b209765bf7e235a
matheusgaliano <galiano.matheus@outlook.com>
2026-01-16 20:46:47 -0300

    feat: add create product crud logic

---
+++ crud.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import Session
+import models
+import schemas
+
+def create_product(db: Session, product: schemas.ProductCreate):
+    keywords_str = product.name.lower()
+    
+    db_product = models.Product(
+        erp_code=product.erp_code,
+        name=product.name,
+        factor_pallet=product.factor_pallet,
+        factor_layer=product.factor_layer,
+        factor_box=product.factor_box,
+        keywords=keywords_str
+    )
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
